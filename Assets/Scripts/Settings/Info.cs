3cb3ffa32ce97153e312e1657f00aff42665efa5
Ethan Entwitle <ethanentwistle@gmail.com>
2018-07-24 14:48:40 -0700

    Added more detail to title and Now fullscreen is the first option

---
+++ Assets/Scripts/Settings/Info.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Info
+{
+    #region PlayerStats
+
+    static int score;
+
+    #endregion
+
+    #region Settings
+    static public bool isFullscreen;
+
+    #endregion
+
+
+
+}
