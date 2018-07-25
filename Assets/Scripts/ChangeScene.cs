48ee1d16fad374631e2e743bc7856cd9c4cdf10d
Ethan Entwitle <ethanentwistle@gmail.com>
2018-07-25 09:25:30 -0700

    Added a change scene script

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int indexOfSceneToLoad;
+    public string nameOfSceneToLoad;
+
+    public void LoadGameScene()
+    {      
+        SceneManager.LoadScene("Main");  
+    }
+
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void LoadSceneWithIndex()
+    {
+        SceneManager.LoadScene(indexOfSceneToLoad);
+    }
+    public void LoadSceneWithName()
+    {
+        SceneManager.LoadScene(nameOfSceneToLoad);
+    }
+
+    public void QuitGame()
+    {     
+       Application.Quit();
+    }
+}
