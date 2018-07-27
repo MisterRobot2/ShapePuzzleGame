2311aadffea0aa22dd19e0fca137ff442ab75b26
Ethan Entwitle <ethanentwistle@gmail.com>
2018-07-27 14:17:06 -0700

    Fixed merge issues and bug fixes

--- Assets/Scripts/Controllers/GameOverScript.cs
+++ Assets/Scripts/Controllers/GameOverScript.cs
@@ -7,33 +7,30 @@ public class GameOverScript : MonoBehaviour
 {
     public GameObject gameOverUIPrefab;
     private AudioSource gameOverSound;
-<<<<<<< HEAD
+
     private int totalScore = 0;
     private int lossCounter = 0;
-=======
+
     private ShapeMovement shapeMovementScript;
-<<<<<<< HEAD
+
     private GameObject heightLine;
     private HeightLine heightLineScript;
-=======
->>>>>>> 7671b9fa843161f5ab9b9b10f0164c0b4532e7ac
->>>>>>> b3484f85711489bf8e2802be7954f3e477d2c68f
 
-    void Start ()
+
+    void Start()
     {
         DebugCheck();
         gameOverUIPrefab.SetActive(false);
         gameOverSound = gameOverUIPrefab.GetComponent<AudioSource>();
-	}
-	
-	void Update ()
+    }
+
+    void Update()
     {
-		
-	}
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< HEAD
         DataBase.score--;
         totalScore = PlayerPrefs.GetInt("Total Blocks Placed") + DataBase.score;
         lossCounter = PlayerPrefs.GetInt("Total Blocks Lost") + 1;
@@ -43,6 +40,7 @@ public class GameOverScript : MonoBehaviour
             DataBase.isGameOver = true;
             GameObject gameOverObject = Instantiate(gameOverUIPrefab);
             gameOverObject.name = "Game Over";
+            heightLineScript.moveDownSequence = true;
             //blocks placed text 
             GameObject.Find("Game Over").transform.Find("Game Over Panel").transform.Find("Blocks Placed Text").GetComponent<Text>().text = "Blocks Placed: " + DataBase.score;
             if (DataBase.score > PlayerPrefs.GetInt("High Score"))
@@ -52,37 +50,23 @@ public class GameOverScript : MonoBehaviour
             GameObject.Find("Game Over").transform.Find("Game Over Panel").transform.Find("High Score Text").GetComponent<Text>().text = "High Score: " + PlayerPrefs.GetInt("High Score");
             PlayerPrefs.SetInt("Total Blocks Placed", totalScore);
             PlayerPrefs.SetInt("Total Blocks Lost", lossCounter);
-=======
-        if(collision.tag == "Block")
-        {
-            shapeMovementScript = collision.gameObject.GetComponent<ShapeMovement>();
->>>>>>> 7671b9fa843161f5ab9b9b10f0164c0b4532e7ac
-        }
-        
-        if (collision.gameObject.tag == "Block" && shapeMovementScript.isFrozen == false)
-        {
-            DataBase.score--;
-            //Create Game Over If 
-            if (!DataBase.isGameOver)
+            if (collision.tag == "Block")
             {
-                DataBase.isGameOver = true;
+                shapeMovementScript = collision.gameObject.GetComponent<ShapeMovement>();
+            }
 
-                GameObject gameOverObject = Instantiate(gameOverUIPrefab);
-                gameOverObject.name = "Game Over";
-                //blocks placed text 
-                GameObject.Find("Game Over").transform.Find("Game Over Panel").transform.Find("Blocks Placed Text").GetComponent<Text>().text = "Blocks Placed: " + DataBase.score;
-                heightLineScript.moveDownSequence = true;
-            }
-            if (DataBase.isGameOver == true)
+            if (collision.gameObject.tag == "Block" && shapeMovementScript.isFrozen == false)
             {
-                gameOverUIPrefab.SetActive(true);
-                gameOverSound.Play();
-                Time.timeScale = 0.3f;
+                if (DataBase.isGameOver == true)
+                {
+                    gameOverUIPrefab.SetActive(true);
+                    gameOverSound.Play();
+                    Time.timeScale = 0.3f;
+                }
             }
+
         }
-        
     }
-
     #region debugCheckFunctions
     void DebugCheck()
     {
