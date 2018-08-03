f7367180ca6dc436efbc4811e07fc8035785e7df
John Nguyen <nguyenduyjohn180@gmail.com>
2018-08-03 14:58:51 -0700

    tutorial panel

--- Assets/Scripts/BackgroundScript.cs
+++ Assets/Scripts/BackgroundScript.cs
@@ -11,6 +11,9 @@ public class BackgroundScript : MonoBehaviour {
     public GameObject starsGameObject;
     ParticleSystem stars;
 
+    public GameObject cloudsGameObject;
+    ParticleSystem clouds;
+
     public GameObject heightText;
     public GameObject goalLineText;
     public GameObject cameraGoalLineText;
@@ -20,6 +23,7 @@ public class BackgroundScript : MonoBehaviour {
         gameObject.GetComponent<Renderer>().material.color = new Color32(0, 252, 255, 255);
         currentColor = gameObject.GetComponent<Renderer>().material.color = new Color32(0, 252, 255, 255);
         stars = starsGameObject.GetComponent<ParticleSystem>();
+        clouds = cloudsGameObject.GetComponent<ParticleSystem>();
     }
 	
 	void Update () {
@@ -27,6 +31,8 @@ public class BackgroundScript : MonoBehaviour {
         if (DataBase.currentHeight >= 40)
         {
             gameObject.GetComponent<Renderer>().material.color = Color.Lerp(currentColor, Color.black, progress);
+            clouds.Stop();
+            cloudsGameObject.SetActive(false);
 
             if (progress < 1)
             {
@@ -42,7 +48,7 @@ public class BackgroundScript : MonoBehaviour {
                 cameraGoalLineText.GetComponent<TextMesh>().color = Color.white;
             }
         }
-        else if(DataBase.currentHeight < 50)
+        else if(DataBase.currentHeight < 40)
         {
             stars.Stop();
             starsGameObject.SetActive(false);
