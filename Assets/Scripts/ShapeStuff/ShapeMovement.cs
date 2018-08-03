5418d701cd85e5401a6cb0e7664dced1ef313974
John Nguyen <nguyenduyjohn180@gmail.com>
2018-08-03 10:14:46 -0700

    Block collides with edge

--- Assets/Scripts/ShapeStuff/ShapeMovement.cs
+++ Assets/Scripts/ShapeStuff/ShapeMovement.cs
@@ -50,12 +50,14 @@ public class ShapeMovement : MonoBehaviour
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
                 this.gameObject.transform.Translate(new Vector3(-movementSpeed * Time.deltaTime, 0, 0),Space.World);
+                this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, -9, 9),this.transform.position.y, this.transform.position.z);
                 rb.constraints = RigidbodyConstraints2D.FreezeRotation;
 
             }
             else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 this.gameObject.transform.Translate(new Vector3(movementSpeed * Time.deltaTime, 0, 0), Space.World);
+                this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, -9, 9), this.transform.position.y, this.transform.position.z);
                 rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             }
             else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.DownArrow) && DataBase.canSpawnShape == true)
