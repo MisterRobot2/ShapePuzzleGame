d73235652baac9b64e6e16a8b99861900520eda7
Ethan Entwitle <ethanentwistle@gmail.com>
2018-08-03 11:02:19 -0700

    removed debug.log

--- Assets/Scripts/ForUI/UpdateGameSpeed.cs
+++ Assets/Scripts/ForUI/UpdateGameSpeed.cs
@@ -12,7 +12,6 @@ public class UpdateGameSpeed : MonoBehaviour
             FirstRun();
         }
         Time.timeScale = DataBase.gameSpeed;
-        Debug.Log(DataBase.gameSpeed);
 	}
 
     void FirstRun()
